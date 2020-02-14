aaa420fdd61c0eb002aa919b40fec50f00229603
RostovNA <nikita.rostovshchikov@mikrokapital.com>
2020-02-14 16:42:07 +0300

    add first extract icon

--- vocab-builder/frontend/src/layouts/MainLayout.vue
+++ vocab-builder/frontend/src/layouts/MainLayout.vue
@@ -1,12 +1,30 @@
 <template>
   <q-layout view="hHh lpR fFf">
 
-    <q-header elevated>
-      <q-toolbar>
-
-      </q-toolbar>
+    <q-header elevated class="header bg-secondary flex">
+        <nav class="navigation flex items-center justify-center self-center">
+          <q-icon name="thumb_up"></q-icon>
+          <a href="" class="flex">
+            <picture>
+              <img src="" alt="Words">
+            </picture>
+            <span>Words</span>
+          </a>
+          <a href="" class="flex">
+            <picture>
+              <img src="" alt="New word">
+            </picture>
+            <span>New</span>
+          </a>
+          <a href="" class="flex">
+            <picture>
+              <img src="" alt="test">
+            </picture>
+            <span>Test</span>
+          </a>
+        </nav>
     </q-header>
-
+    <q-btn icon="thumbs_up" />
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -27,3 +45,13 @@ export default {
   }
 }
 </script>
+
+<style>
+  .header{
+    height:75px;
+  }
+  .navigation{
+    height:100%;
+    width:100%;
+  }
+</style>
