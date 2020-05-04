bc1191ab87b93a2c0a33218155e80e4f09164ebd
ViKeyNs <32480582+ViKeyNs@users.noreply.github.com>
2020-05-04 15:35:06 +0500

    new server tests

---
+++ vocab-builder/server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+global.Vocab = require('./api/models/vocabModel');
+const routes = require('./api/routes/vocabRoutes');
+
+mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
+mongoose.connect(
+  'mongodb://localhost/vocab-builder',
+  { useNewUrlParser: true }
+);
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+app.use((req, res) => {
+  res.status(404).send({ url: `${req.originalUrl} not found` });
+});
+
+module.exports = app
